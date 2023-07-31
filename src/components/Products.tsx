25e2ce70d0bdd6e0736a6d0272cc3843aef8eb31
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-07-30 19:52:28 -0600

    first commit

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 import Card from './Card';
 import SearchBar from './SearchBar';
 
-
 export interface ProductData {
   id: number;
   title: string;
@@ -15,11 +14,12 @@ export interface ProductData {
   category: string;
 }
 
-
 const Products = () => {
   const [products, setProducts] = useState<ProductData[]>([]);
   const [filteredProducts, setFilteredProducts] = useState<ProductData[]>([]);
   const [showNoResultsMessage, setShowNoResultsMessage] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,6 +35,19 @@ const Products = () => {
     };
 
     fetchData();
+  }, []);
+
+  useEffect(() => {
+    setFilteredProducts(products.slice(0, currentPage * 10));
+  }, [products, currentPage]);
+
+  useEffect(() => {
+    
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleSearch = (
@@ -56,19 +69,35 @@ const Products = () => {
       filteredResults = filteredResults.filter((product) => product.price < 100);
     }
 
-    setFilteredProducts(filteredResults);
+    setFilteredProducts(filteredResults.slice(0, currentPage * 10));
     setShowNoResultsMessage(filteredResults.length === 0);
   };
 
+  const handleScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 50 &&
+      !loading
+    ) {
+      setLoading(true);
+      setCurrentPage((prevPage) => prevPage + 1);
+      setLoading(false);
+    }
+  };
+
   return (
-    <section className="p-8 animate__animated animate__fadeIn" id="products" style={{ paddingBottom: '200px' }}>
+    <section
+      className="p-8 animate__animated animate__fadeIn"
+      id="products"
+      style={{ paddingBottom: '200px' }}
+    >
       <h2 className="text-center text-purple-800 text-4xl font-bold pb-16">Products</h2>
 
       <SearchBar products={products} onSearch={handleSearch} />
 
       {showNoResultsMessage ? (
-        <p className="text-red-500 flex justify-center items-center h-full">No results found. Please try a different search term.</p>
-
+        <p className="text-red-500 flex justify-center items-center h-full">
+          No results found. Please try a different search term.
+        </p>
       ) : (
         <div className="flex justify-center">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-screen-xl">
@@ -83,15 +112,13 @@ const Products = () => {
                 image={product.image}
                 rating={product.rating}
               />
-            ))
-            }
+            ))}
           </div>
         </div>
       )}
+      {loading && <p>Loading...</p>}
     </section>
   );
 };
 
 export default Products;
-
-
